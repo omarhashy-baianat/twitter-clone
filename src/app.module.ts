6d3ebf2ba1b447b2dc9aace47f12ac249732b96d
omarhashy <omarhashy.baianat@gmail.com>
2025-04-06 18:50:47 +0200

    connect to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,41 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
 import { TweetsModule } from './tweets/tweets.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env"
+    })
+    ,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql',
     }),
+    TypeOrmModule.forRootAsync(
+      {
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) =>  {
+          return {
+            type: 'postgres',
+            host: config.get('POSTGRES_HOST'),
+            port: config.get('POSTGRES_PORT'),
+            username: config.get('POSTGRES_USER'),
+            password: config.get('POSTGRES_PASSWORD'),
+            entities: [],
+            database: config.get<string>("POSTGRES_DB"),
+            synchronize : true
+          }
+        }
+      }
+    ),
     UsersModule,
     TweetsModule,
     AuthModule,
-
   ],
   providers: [AppService],
 })
