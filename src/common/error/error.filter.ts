6f633290a0b16a6de3d7de0cf2e0341f992d56d1
omarhashy <omarhashy.baianat@gmail.com>
2025-04-08 10:11:30 +0200

    add success to the error message

--- src/common/error/error.filter.ts
+++ src/common/error/error.filter.ts
@@ -1,39 +1,39 @@
 // filters/graphql-exception.filter.ts
 import {
-    ArgumentsHost,
-    Catch,
-    ExceptionFilter,
-    HttpException,
-    HttpStatus,
-  } from '@nestjs/common';
-  import { GqlArgumentsHost } from '@nestjs/graphql';
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { GqlArgumentsHost } from '@nestjs/graphql';
 import { timeStamp } from 'console';
-  
-  @Catch()
-  export class GraphQLExceptionFilter implements ExceptionFilter {
-    catch(exception: unknown, host: ArgumentsHost) {
-      const gqlHost = GqlArgumentsHost.create(host);
-      const ctx = gqlHost.getContext();
-      const response = ctx.res as any;  
-  
-      let status = HttpStatus.INTERNAL_SERVER_ERROR;
-      let message = 'Internal server error';
-      
-      if (exception instanceof HttpException) {
-        status = exception.getStatus();
-        const response = exception.getResponse() as any;
-        if (typeof response === 'string') {
-          message = response;
-        } else if (typeof response === 'object' && response !== null) {
-          message = response.message || message;
-        }
+
+@Catch()
+export class GraphQLExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const gqlHost = GqlArgumentsHost.create(host);
+    const ctx = gqlHost.getContext();
+    const response = ctx.res as any;
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal server error';
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const response = exception.getResponse() as any;
+      if (typeof response === 'string') {
+        message = response;
+      } else if (typeof response === 'object' && response !== null) {
+        message = response.message || message;
       }
-  
-      response.status(status).json({
-        timeStamp: new Date().toISOString(),
-        statusCode: status,
-        message,
-      });
     }
+
+    response.status(status).json({
+      success: false,
+      timeStamp: new Date().toISOString(),
+      statusCode: status,
+      message,
+    });
   }
-  
+}
