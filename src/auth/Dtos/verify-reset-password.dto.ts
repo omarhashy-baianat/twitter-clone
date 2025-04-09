be642d86540b63676db1a23bd70a62ed7e7391ed
omarhashy <omarhashy.baianat@gmail.com>
2025-04-09 18:49:04 +0200

    verify reset password dto

---
+++ src/auth/Dtos/verify-reset-password.dto.ts
@@ -0,0 +1,33 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { Transform } from 'class-transformer';
+import {
+  IsAlphanumeric,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+@InputType()
+export class verifyResetPassword {
+  @IsEmail()
+  @Transform(({ value }) => value.trim())
+  @Field()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim())
+  @Field()
+  otp: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  @MinLength(5)
+  @IsAlphanumeric()
+  @Transform(({ value }) => value.trim())
+  @Field()
+  password: string;
+}
